f24c1290905383eef55f1e6ff52b873fe2c40a88
mdnahidbhuyaan <mdnahidbuiyan14@gmail.com>
2026-02-26 22:05:06 +0600

    navbar end

--- src/Components/ShardComponents/Header/MinHeader.jsx
+++ src/Components/ShardComponents/Header/MinHeader.jsx
@@ -1,9 +1,47 @@
 import React from 'react'
+import nahidLogo from "../../../assets/image/nahidLogo.png"
+import { CiUser } from 'react-icons/ci'
+import { BiShoppingBag } from 'react-icons/bi'
 
 const MinHeader = () => {
   return (
-    <div>
-      <h1>MinHeader</h1>
+    <div className='bg-white'>
+      <div className='flex items-center justify-between container mx-auto px-24'>
+        <img className='w-22' src={nahidLogo} alt="" />
+        <div>
+<label className="input outline-none">
+  <svg className="h-[1.5em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+    <g
+      strokeLinejoin="round"
+      strokeLinecap="round"
+      strokeWidth="2.5"
+      fill="none"
+      stroke="currentColor"
+    >
+      <circle cx="11" cy="11" r="8"></circle>
+      <path d="m21 21-4.3-4.3"></path>
+    </g>
+  </svg>
+  <input type="search " className='pr-48' required placeholder="Search" />
+</label>
+        </div>
+        <div className='flex items-center gap-5'>
+          <div className='flex items-center gap-1'>
+            <CiUser className='text-black text-2xl'/>
+            <div>
+              <p className='text-black ct text-xs'>Account</p>
+              <p className='text-black ts'>Login</p>
+            </div>
+          </div>
+                 <div className='flex items-center gap-1'>
+            <BiShoppingBag  className='text-black text-2xl'/>
+            <div>
+              <p className='text-black ct text-xs'>Account</p>
+              <p className='text-black ts'>Login</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/Components/ShardComponents/Header/TopHeader.jsx
+++ src/Components/ShardComponents/Header/TopHeader.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import { FiPhoneCall } from "react-icons/fi";
+import { PiWhatsappLogo } from 'react-icons/pi';
+
 
 const TopHeader = () => {
   return (
-    <div>
-      <h1>TopHeader</h1>
+    <div className='bg-[#f8f8fb] py-2'>
+     <div className='flex justify-between items-center container mx-auto px-24'>
+              <div className='flex items-center gap-12 ct'>
+        <div className='flex items-center gap-2'>
+            <FiPhoneCall />
+            <p className='text-[12px] ct'>+91 123456789</p>
+        </div>
+        <div className='flex items-center gap-2'>
+                  <PiWhatsappLogo />
+                    <p className='text-[12px] '>01425636872</p>
+        </div>
+      </div>
+      <div>
+        <p className='text-[12px] ct'>World's fastest Online Shopping Destination</p>
+      </div>
+      <div className='flex items-center ct text-[12px] gap-5'>
+        <p>Help?</p>
+        <p>Trac Order?</p>
+        <p>English</p>
+        <p>Contact US</p>
+      </div>
+     </div>
     </div>
   )
 }
