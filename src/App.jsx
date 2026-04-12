24ed67a519d28101d04ea186f11a7ccb5bf7f90e
mdnahidbhuyaan <mdnahidbuiyan14@gmail.com>
2026-04-12 22:20:06 +0600

    clear

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,7 @@ import React from "react";
 const App = () => {
   return <div className="text-4xl">App
   
-  <h1>Hello</h1>
-  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus delectus molestias laboriosam voluptatum, officiis tempore facere eaque pariatur similique! Exercitationem.</p>
-  <h1></h1>
+<h1></h1>
   </div>;
 };
 
